044daa766a31c110344ef425e63e2191db7a9a0d
LanSanter <30773991+LanSanter@users.noreply.github.com>
2025-01-15 18:44:51 +0800

    translate chinese

--- MM07.c
+++ MM07.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-// Function to count occurrences of N in M
+// 函數：計算 N 在 M 中出現的次數
 int count_occurrences(int n, long long m) {
     char str_n[3], str_m[8];
 
-    // Convert N and M to strings
+    // 將 N 和 M 轉換為字串
     sprintf(str_n, "%d", n);
     sprintf(str_m, "%lld", m);
 
     int count = 0;
     char *pos = str_m;
 
-    // Find occurrences of N in M
+    // 找到 N 在 M 中的出現次數
     while ((pos = strstr(pos, str_n)) != NULL) {
         count++;
-        pos++; // Move pointer to continue search
+        pos++; // 指標向後移動，繼續搜尋
     }
 
     return count;
@@ -25,13 +25,11 @@ int main() {
     int n;
     long long m;
 
-    // Read the inputs N and M
-    while(scanf("%d %lld", &n, &m) != EOF) {
-		printf("%d\n", count_occurrences(n, m));
-		// Calculate and print the result
-	};
+    // 讀取輸入的 N 和 M
+    while (scanf("%d %lld", &n, &m) != EOF) {
+        // 計算並輸出結果
+        printf("%d\n", count_occurrences(n, m));
+    }
 
-    
-    
     return 0;
 }
